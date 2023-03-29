f1749cacd449b5617b7950224ad9ef4cb799d49a
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-03-29 15:59:52 +0100

    2. Movimento base esquerda/direita na entrada

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        horizontalInput = Input.GetAxis("Horizontal");
         // Mover o veículo para frente
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed );
