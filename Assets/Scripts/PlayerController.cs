a9946947f142a51c6944d9fd670e6e915e74b58a
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-03-29 15:47:48 +0100

    1. Permita que o veículo se mova para a esquerda/direita

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,6 @@ public class PlayerController : MonoBehaviour
         // Mover o veículo para frente
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed );
-        
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
