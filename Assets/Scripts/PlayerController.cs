a76a8e2876814e1ac6a878936433aae7a8ae2613
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-03-29 23:14:38 +0100

    5. Limpe seu código e hierarquia

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    // private variables 
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +19,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // this is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+
         // Mover o veículo para frente
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput );
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // we turn the vihacle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 
