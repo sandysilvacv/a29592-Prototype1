569d36006fd8b3bcf0897fc5b66bd4df4cdb5865
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-03-29 22:41:51 +0100

    3. Assuma o controle da velocidade do veículo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 20;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,10 @@ public class PlayerController : MonoBehaviour
     {
 
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Mover o veículo para frente
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput );
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
