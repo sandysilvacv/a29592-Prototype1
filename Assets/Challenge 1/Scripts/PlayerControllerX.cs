9441c3bd01b05388fa8f5de2d714e7d9fd50cae1
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-03-31 12:24:35 +0100

    mudança de posição e rotação

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 20;
+    public float speed = 0.1f;
     public float rotationSpeed;
     public float verticalInput;
     public float HorizontalInput;
@@ -22,7 +22,7 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         HorizontalInput = Input.GetAxis("Horizontal");
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput * HorizontalInput * speed);
