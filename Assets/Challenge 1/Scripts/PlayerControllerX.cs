d9fa7642595b478f1c8da7d1b0415b132b01f9c4
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-03-31 12:57:16 +0100

    Mudei as variáveis de privado para publico

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,7 +7,7 @@ public class PlayerControllerX : MonoBehaviour
     public float speed = 0.1f;
     public float rotationSpeed;
     public float verticalInput;
-    public float HorizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +20,11 @@ public class PlayerControllerX : MonoBehaviour
         // get the user's vertical input
 
         verticalInput = Input.GetAxis("Vertical");
-        HorizontalInput = Input.GetAxis("Horizontal");
+       
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput * HorizontalInput * speed);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput );
     }
 }
