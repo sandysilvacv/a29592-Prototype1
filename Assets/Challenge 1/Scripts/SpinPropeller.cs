f700cfb9d8352dda1d08e0f4620111236c7a9397
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-04-04 16:37:13 +0100

    Bônus - Faça a hélice girar

--- Assets/Challenge 1/Scripts/SpinPropeller.cs
+++ Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SpinPropeller : MonoBehaviour
 {
+
+    public Vector3 axisRotate;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // transform.Rotate(new Vector3(x: 0, y: 1, z: 0)); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(axisRotate * speed * Time.deltaTime);
     }
 }
