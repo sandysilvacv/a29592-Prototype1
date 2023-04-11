c2503c05ed12afdb22f58a7cc6075a3926c6f777
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-04-11 23:38:11 +0100

    Recursos de bônus 1 - Compartilhe seu trabalho; Fácil : Pirâmides de Obstáculos

--- Assets/Challenge 1/Scripts/SpinPropeller.cs
+++ Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -10,7 +10,7 @@ public class SpinPropeller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       // transform.Rotate(new Vector3(x: 0, y: 1, z: 0)); 
+       
     }
 
     // Update is called once per frame
